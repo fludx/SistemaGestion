eece983d1852823da4090a104c1ff16c4e1a9e22
fludx <rossisantiago724@gmail.com>
2025-10-31 13:22:56 -0300

    ya estan todos los od y dto

---
+++ Datos/DTOs Bitacora/BitacoraDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DTOs_Stock
+{
+    public class BitacoraDTO
+    {
+        public int IdUsuario { get; set; }
+        public string Accion { get; set; }
+        public string Modulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
--- Datos/Od Bitacora/Od_RegistrarBitacora.cs
+++ Datos/Od Bitacora/Od_RegistrarBitacora.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Datos.Conecction;
+using Datos.DTOs_Stock;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Datos.Od_Bitacora
+namespace Datos.Od_Stock
 {
-    internal class Od_RegistrarBitacora
+    public class Od_RegistrarBitacora : Ejeconsultas_Stock
     {
+        public bool RegistrarBitacora(BitacoraDTO bitacora)
+        {
+            try
+            {
+                string nombreSP = "sp_RegistrarBitacora";
+
+                // Lista de parámetros
+                List<SqlParameter> parametros = new List<SqlParameter>
+                {
+                    new SqlParameter("@id_usuario", SqlDbType.Int) { Value = bitacora.IdUsuario },
+                    new SqlParameter("@accion", SqlDbType.NVarChar, 100) { Value = bitacora.Accion },
+                    new SqlParameter("@modulo", SqlDbType.NVarChar, 100) { Value = (object)bitacora.Modulo ?? DBNull.Value },
+                    new SqlParameter("@descripcion", SqlDbType.NVarChar, 500) { Value = (object)bitacora.Descripcion ?? DBNull.Value },
+                    new SqlParameter("@fecha_registro", SqlDbType.DateTime2) { Value = bitacora.FechaRegistro }
+                };
+
+                SqlParameter[] sqlParam = parametros.ToArray();
+
+                // Ejecutar el procedimiento almacenado
+                DataTable dt = EjecConsultas(nombreSP, sqlParam);
+
+                // Si devuelve filas, la operación fue exitosa
+                return dt.Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al registrar en la bitácora: " + ex.Message);
+            }
+        }
     }
 }
